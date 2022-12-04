f3d52ab3cbb07b5a0b3185256a39d4b2b4dab866
Bohdan <protsysyn.bohdan@gmail.com>
2022-12-04 08:45:11 +0200

    added footer

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,7 +1,40 @@
-import React from 'react'
-
+import { Container } from "@mui/system";
+import React from "react";
+import { Grid } from "@mui/material";
+import { SocialIcon } from "react-social-icons";
+import "./css/Footer.css";
 export default function () {
   return (
-    <div></div>
-  )
+    <Container
+      maxWidth="md"
+      sx={{ bgcolor: "yellow", borderTop: 3, height: "20%" }}
+    >
+      <Grid
+        container
+        justifyContent="space-between"
+        flexDirection={{ sm: "column", md: "row" }}
+      >
+        <div style={{ textAlign: "left" }}>
+          <span style={{ fontSize: 25 }}>Contacts:</span>
+          <span style={{ fontSize: 20 }}>
+            <br></br>Address: Ivana Fedorova street, 10
+          </span>
+          <span style={{ fontSize: 20 }}>
+            <br></br>Phone number: 050-458-34-41
+          </span>
+        </div>
+        <div className="networks">
+          <div>
+            <SocialIcon network="twitter" />
+          </div>
+          <div>
+            <SocialIcon network="facebook" />
+          </div>
+          <div>
+            <SocialIcon network="instagram" />
+          </div>
+        </div>
+      </Grid>
+    </Container>
+  );
 }
